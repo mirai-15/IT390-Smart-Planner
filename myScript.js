7efc86d89363ed0599e5deeff981f26733855bcf
Minsung Kim <107968809+MinsungKim-G01209828@users.noreply.github.com>
2025-09-24 20:30:11 -0400

    Update myScript.js
    Minsung's website with a login feature

--- myScript.js
+++ myScript.js
@@ -1,3 +1,24 @@
 function displayPricing() {
     alert("Basic Plan: Free\nPremium Plan: $15.99/month\nMore details on the pricing page.");
 }
+
+function startLoading (){
+    let loading_page = document.getElementById("loading_page");
+    let progress_bar = document.getElementById("bar");
+
+    let value = 0;
+    let loading = setInterval(()=>{
+        value += 5;
+        progress_bar.style.width = value + "%";
+        if (value >= 100){
+            clearInterval(loading);
+            loading_page.style.display = "none";
+            app.style.display = "block"; 
+
+        }
+    }, 150);
+}
+
+if (document.title == "Try Smart Planner"){
+    window.onload = startLoading;
+}
