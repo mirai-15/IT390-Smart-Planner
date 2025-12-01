709ec1c57514adcb399efa41b799b9254d57497e
mirai-15 <165509491+mirai-15@users.noreply.github.com>
2025-12-01 16:25:48 -0500

    Upgrade Firebase SDK to version 8.10.1

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,23 +1,68 @@
-importScripts("https://www.gstatic.com/firebasejs/12.3.0/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/12.3.0/firebase-messaging-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js");
+importScripts(
+  "https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js"
+);
 
-firebase.initializeApp({
+// Replace these with your own Firebase config keys...
+const firebaseConfig = {
   apiKey: "AIzaSyBNwZRftT8Cn8DufLbmddKHvK0lMhRRxlU",
   authDomain: "smartplanner-f2540.firebaseapp.com",
   projectId: "smartplanner-f2540",
+  storageBucket: "smartplanner-f2540.firebasestorage.app",
   messagingSenderId: "469173527661",
-  appId: "1:469173527661:web:b27cff5519aedd4a338b64"
-});
+  appId: "1:469173527661:web:b27cff5519aedd4a338b64",
+  measurementId: "G-7Y8EZVY7M0"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-  console.log("[firebase-messaging-sw.js] Received background message: ", payload);
+  console.log(
+    "[firebase-messaging-sw.js] Received background message ",
+    payload
+  );
 
-  const notificationTitle = payload.notification?.title || "SmartPlanner";
+  // payload.fcmOptions?.link comes from our backend API route handle
+  // payload.data.link comes from the Firebase Console where link is the 'key'
+  const link = payload.fcmOptions?.link || payload.data?.link;
+
+  const notificationTitle = payload.notification.title;
   const notificationOptions = {
-    body: payload.notification?.body
+    body: payload.notification.body,
+    icon: "./logo.png",
+    data: { url: link },
   };
-  
   self.registration.showNotification(notificationTitle, notificationOptions);
+});
+
+self.addEventListener("notificationclick", function (event) {
+  console.log("[firebase-messaging-sw.js] Notification click received.");
+
+  event.notification.close();
+
+  // This checks if the client is already open and if it is, it focuses on the tab. If it is not open, it opens a new tab with the URL passed in the notification payload
+  event.waitUntil(
+    clients
+      // https://developer.mozilla.org/en-US/docs/Web/API/Clients/matchAll
+      .matchAll({ type: "window", includeUncontrolled: true })
+      .then(function (clientList) {
+        const url = event.notification.data.url;
+
+        if (!url) return;
+
+        // If relative URL is passed in firebase console or API route handler, it may open a new window as the client.url is the full URL i.e. https://example.com/ and the url is /about whereas if we passed in the full URL, it will focus on the existing tab i.e. https://example.com/about
+        for (const client of clientList) {
+          if (client.url === url && "focus" in client) {
+            return client.focus();
+          }
+        }
+
+        if (clients.openWindow) {
+          console.log("OPENWINDOW ON CLIENT");
+          return clients.openWindow(url);
+        }
+      })
+  );
 });
